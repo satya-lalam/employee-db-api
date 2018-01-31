d7a113340173f2ea2294e998abb3319b8c0270ce
slalam <satyakumar.lalam@unmc.edu>
2018-01-31 17:29:38 -0600

    allowing cross origin requests

--- src/main/java/com/firm/employeedbapi/controller/WebController.java
+++ src/main/java/com/firm/employeedbapi/controller/WebController.java
@@ -3,6 +3,7 @@ package com.firm.employeedbapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.firm.employeedbapi.beans.Employee;
 import com.firm.employeedbapi.service.EmployeeService;
 
+@CrossOrigin
 @RestController
 public class WebController {
 	
